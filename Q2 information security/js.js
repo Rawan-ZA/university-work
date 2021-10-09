00adb4f2fb82656d372c25d7abb15f3c7c675645
Rawan Azzam <77897243+Rawan-ZA@users.noreply.github.com>
2021-10-09 13:43:11 +0300

    Update js.js

--- Q2 information security/js.js
+++ Q2 information security/js.js
@@ -2,21 +2,18 @@
     function forceAttack(){
       var enter_pass = document.getElementById('password').value;
       var pass = Math.round(Math.random()*100000+1);
-      var attempts = 0
       const start = Date.now();
      for (let i = 0; i < 99999; i++){
        if(enter_pass != pass || enter_pass.length != '5'){
         alert('Password is Wrong try again');
-        attempts++;
+        var enter_pass = document.getElementById('password').value = '';
+        window.open('index.html');
         break;
        }
        else if(enter_pass == pass ){
         alert('Password is true login  seccuss');
         break;
       }
-        if( attempts == 3){
-          alert(denide);
-        }
         }
       const stop = Date.now();
       alert(`Time Taken to  matching password =  ${Math.round((stop - start)/1000)}  seconds`); 
